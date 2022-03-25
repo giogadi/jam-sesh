df0680499dbea1cb4ca4ef19692c5a95faf38cb5
Luis G. Torres <lgtorres42@gmail.com>
2022-03-24 21:41:27 -0500

    Wait for click before enabling audio to avoid warnings. Also enables serverless?

--- client/script.js
+++ client/script.js
@@ -1,39 +1,53 @@
-let jamModel = new JamModel();
-let jamView = new JamView(document.body);
+async function main() {
+    // Let's wait for a mouse click before doing anything
+    let msg = document.getElementById('message');
+    msg.innerHTML = 'Click to start';
+    const waitForClick = () =>
+        new Promise((resolve) => {
+            window.addEventListener('click', () => resolve(), {once: true});
+        });
+    await waitForClick();
+    msg.innerHTML = '';
 
-// method of JamModel
-function stateChange() {
-    let cellSequence = [];
-    for (let i = 0; i < this.synthSequence.length; ++i) {
-        let voices = [];
-        let numVoices = this.synthSequence[0].length;
-        for (let voiceIx = 0; voiceIx < numVoices; ++voiceIx) {
-            if (this.synthSequence[i][voiceIx] < 0) {
-                voices.push(-1);
-            } else {
-                voices.push(fromNoteIxToCellIx(this.currentScale, this.synthSequence[i][voiceIx]));
+    let jamModel = new JamModel();
+    let jamView = new JamView(document.body);
+
+    // method of JamModel
+    function stateChange() {
+        let cellSequence = [];
+        for (let i = 0; i < this.synthSequence.length; ++i) {
+            let voices = [];
+            let numVoices = this.synthSequence[0].length;
+            for (let voiceIx = 0; voiceIx < numVoices; ++voiceIx) {
+                if (this.synthSequence[i][voiceIx] < 0) {
+                    voices.push(-1);
+                } else {
+                    voices.push(fromNoteIxToCellIx(this.currentScale, this.synthSequence[i][voiceIx]));
+                }
             }
+            cellSequence.push(voices);
         }
-        cellSequence.push(voices);
+
+        let beatIndex = this.playback.playIntervalId === null ? -1 : this.playback.beatIndex;
+
+        let viewModel = {
+            synthSequence: cellSequence,
+            drumSequence: this.drumSequence,
+            currentBeatIndex: beatIndex,
+            bpm: this.playback.bpm,
+            scale: this.currentScale
+        };
+        jamView.updateView(viewModel);
     }
 
-    let beatIndex = this.playback.playIntervalId === null ? -1 : this.playback.beatIndex;
+    jamView.onClickUpdateSynthSequence = jamModel.updateSynthSequence.bind(jamModel);
+    jamView.onClickUpdateDrumSequence = jamModel.updateDrumSequence.bind(jamModel);
+    jamView.changeBpm = changeBpm.bind(jamModel);
+    jamView.changeScale = scaleChanged.bind(jamModel);
+    jamView.togglePlayback = jamModel.togglePlayback.bind(jamModel);
+    jamModel.stateChange = stateChange.bind(jamModel);
 
-    let viewModel = {
-        synthSequence: cellSequence,
-        drumSequence: this.drumSequence,
-        currentBeatIndex: beatIndex,
-        bpm: this.playback.bpm,
-        scale: this.currentScale
-    };
-    jamView.updateView(viewModel);
+    jamModel.forceStateUpdate();
 }
 
-jamView.onClickUpdateSynthSequence = jamModel.updateSynthSequence.bind(jamModel);
-jamView.onClickUpdateDrumSequence = jamModel.updateDrumSequence.bind(jamModel);
-jamView.changeBpm = changeBpm.bind(jamModel);
-jamView.changeScale = scaleChanged.bind(jamModel);
-jamView.togglePlayback = jamModel.togglePlayback.bind(jamModel);
-jamModel.stateChange = stateChange.bind(jamModel);
-
-jamModel.forceStateUpdate();
+main();
